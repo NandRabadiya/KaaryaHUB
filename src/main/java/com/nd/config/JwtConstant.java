04c222db88c81655902f81b57827cd8c2fc6998a
Nand Rabadiya <nandrabadiya2003@gmail.com>
2024-12-20 00:39:07 +0530

    Config Files

---
+++ src/main/java/com/nd/config/JwtConstant.java
@@ -0,0 +1,9 @@
+package com.nd.config;
+
+//import jdk.internal.icu.impl.ICUBinary;
+
+public class JwtConstant {
+
+    public static final String SECRET_KEY= "jdfjakjehnhrio8uw8ejrbdjhhfuigejrhsgh";
+    public static final String JWT_HEADER="Authorization";
+}
---
+++ src/main/java/com/nd/modal/User.java
@@ -0,0 +1,4 @@
+package com.nd.modal;
+
+public class User {
+}
