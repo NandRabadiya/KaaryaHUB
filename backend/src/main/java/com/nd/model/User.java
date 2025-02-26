b557db51b05dc28925d7a26402660a4a13fdedf4
VenuVirparia <venupatel004@gmail.com>
2025-02-26 22:55:53 +0530

    API tested

--- backend/src/main/java/com/nd/model/User.java
+++ backend/src/main/java/com/nd/model/User.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -22,6 +23,7 @@ public class User {
 
     private String fullName;
     private String email;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
     private String role;
