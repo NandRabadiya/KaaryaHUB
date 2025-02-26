3bf65356bb0862ba68208877686ddd62b0be2e9b
Nand <nandrabadiya2003@gmail.com>
2025-02-26 16:27:21 +0530

    Razor Pay integraated and lombok fixed.

--- backend/src/main/java/com/nd/request/CreateCommentRequest.java
+++ backend/src/main/java/com/nd/request/CreateCommentRequest.java
@@ -1,9 +1,29 @@
 package com.nd.request;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Data
+@Getter
+@Setter
 public class CreateCommentRequest {
     private Long issueId;
     private String content;
+
+    public Long getIssueId() {
+        return issueId;
+    }
+
+    public void setIssueId(Long issueId) {
+        this.issueId = issueId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
--- backend/src/main/java/com/nd/response/MessageResponse.java
+++ backend/src/main/java/com/nd/response/MessageResponse.java
@@ -1,13 +1,18 @@
 package com.nd.response;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
 public class MessageResponse {
     private String message;
 
+    public void setMessage(String commentDeletedSuccessfully) {
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
