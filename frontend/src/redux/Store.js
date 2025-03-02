d00c08712d5c85f4e2beb35501912855c969618d
Nand <nandrabadiya2003@gmail.com>
2025-03-03 01:13:22 +0530

    redux for Project

--- frontend/src/redux/Store.js
+++ frontend/src/redux/Store.js
@@ -1,8 +1,10 @@
 import { applyMiddleware, legacy_createStore } from "redux";
 import authReducer from "./Auth/Reducer";
+import ProjectReducer from "./Project/Project.Reducer";
 
 const rootReducer = combineReducers({
-    auth:authReducer
+    auth:authReducer,
+    project:ProjectReducer
 });
 
 
