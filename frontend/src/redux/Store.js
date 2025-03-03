069b757d6af3643c2a47be0a8664c93f684bff0b
Venu <venupatel004@gmail.com>
2025-03-03 19:10:45 +0530

    Chat , Comment and Issue Reducer added

--- frontend/src/redux/Store.js
+++ frontend/src/redux/Store.js
@@ -1,10 +1,17 @@
 import { applyMiddleware, legacy_createStore } from "redux";
 import authReducer from "./Auth/Reducer";
 import ProjectReducer from "./Project/Project.Reducer";
+import { comment } from "postcss";
+import issueReducer from "./Issue/Issue.reducer";
+import ChatReducer from "./Chat/Reducer";
+import commentReducer from "./Comment/comment.reducer";
 
 const rootReducer = combineReducers({
     auth:authReducer,
-    project:ProjectReducer
+    project:ProjectReducer,
+    chat:ChatReducer,
+    issue:issueReducer,
+    comment:commentReducer
 });
 
 
