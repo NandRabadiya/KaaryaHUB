3babe799aa5e778865a7279dab48f2c6932f5ef6
Nand <nandrabadiya2003@gmail.com>
2025-03-11 23:36:56 +0530

    Payment redux added.

--- frontend/src/pages/Subscription/Subscription.jsx
+++ frontend/src/pages/Subscription/Subscription.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import SubscriptionCard from "./SubscriptionCard";
 import { useSelector, useDispatch } from "react-redux";
-// import { getUserSubscription } from "@/redux/Subscription/Action";
+import { getUserSubscription } from "@/redux/Subscription/Action";
 
 const paidPlan = [
   "Add unlimited project",
